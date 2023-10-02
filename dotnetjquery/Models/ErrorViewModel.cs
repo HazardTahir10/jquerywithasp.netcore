0d72d3bb3cf52164ecfa960e9408fce7fa307bda
tahirhussain2213 <Tahirhussain2213@gmail.com>
2023-10-02 12:28:37 +0545

    first commit

---
+++ dotnetjquery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetjquery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

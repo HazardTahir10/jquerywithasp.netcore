97af23572b35a21dcd9a60550a4ff75131d7d2ae
tahirhussain2213 <Tahirhussain2213@gmail.com>
2023-10-02 13:50:08 +0545

    Passing form data to controller using ajax

--- dotnetjquery/Controllers/HomeController.cs
+++ dotnetjquery/Controllers/HomeController.cs
@@ -17,7 +17,24 @@ namespace dotnetjquery.Controllers
         {
             return View();
         }
+        public int Add(int num1, int num2)
+        {
+            return num1+ num2;
+        }
 
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Mul (int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
         public IActionResult Privacy()
         {
             return View();
